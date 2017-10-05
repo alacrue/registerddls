0edd1512b5e9f9b6a3946380eefc9b0cd4b89571
alacrue <arlacrue@uark.edu>
2017-10-04 22:59:44 -0500

    created employee table

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -11,6 +11,43 @@ CREATE TABLE product (
   OIDS=FALSE
 );
 
+CREATE TABLE employee (
+  record_id uuid NOT NULL,
+  f_name char(10) NOT NULL DEFAULT(''),
+  l_name char(10) NOT NULL DEFAULT(''),
+  employee_id int UNIQUE,
+  active char(10) CHECK (active IN ('active','inactive')),
+  role char(32) CHECK (role IN ('cashier','shift manager','manager')),
+  manager int REFERENCES employee(employee_id),
+  password char(15) NOT NULL DEFAULT(''),
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT employee_pkey PRIMARY KEY (record_id)
+);
+
+INSERT INTO employee VALUES (
+  uuid_generate_v4(),
+  'John',
+  'Doe',
+  '1',
+  'active',
+  'cashier',
+  '3',
+  'abcd',
+  current_timestamp
+);
+
+INSERT INTO employee VALUES (
+  uuid_generate_v4(),
+  'Rachel',
+  'Greene',
+  '3',
+  'active',
+  'manager',
+  NULL,
+  'defg',
+  current_timestamp
+);
+
 CREATE INDEX ix_product_lookupcode
   ON product
   USING btree
